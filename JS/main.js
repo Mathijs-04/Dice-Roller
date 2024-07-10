5cb3dc2b25e8eedbd670bd10d27c587154f39d7a
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-07-10 11:37:44 +0200

    form added

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+// Pseudocode Dice Roller
+
+// Choose the number of dice to roll
+
+// Choose the number of sides on the dice
+
+// Choose what numbers make a successfull roll
+
+// Roll the dice
+
+// Count the number of successful rolls
+
+// Display the number of successful rolls
+
+// Display the total rolls
+
+// Roll again with the successfull rolls
